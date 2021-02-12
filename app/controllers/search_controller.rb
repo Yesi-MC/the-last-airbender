e80d5224e3c0f2d592f9950447dfebfa2330ac7d
Yesi Meza <yesi.meza10@gmail.com>
2021-02-12 10:47:16 -0700

    edited the controller to include facade

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,8 @@
 class SearchController < ApplicationController 
   def index 
     nation_name = params[:nation].gsub("_", "+")
-    conn = Faraday.new("https://last-airbender-api.herokuapp.com")
-    response = conn.get("api/v1/characters?affiliation=#{nation_name}&perPage=25")
-    @characters = JSON.parse(response.body, symbolize_names: true )
+
+    @characters = SearchFacade.character_info(nation_name)
   end
 end
 
